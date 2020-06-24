43d07cbb154b64595ab72cca889a3c305e4c59ca
mcastiglione <marcocastiglione5@gmail.com>
2020-06-24 11:21:45 -0500

    audio player improved with smooth bar animation

--- src/components/MessagesDisplay/components/Audio.js
+++ src/components/MessagesDisplay/components/Audio.js
@@ -1,19 +1,21 @@
 import React, { useRef, useState, useEffect } from 'react';
 import style from './Audio.module.css';
 
-export default function Audio(props, ...rest) {
+export default function Audio(props) {
     let audioRef = useRef();
     let [length, setLength] = useState(null);
-    let [progress, setProgress] = useState(null);
+    let [progress, setProgress] = useState(null);       let progressInterval;
     let [count, setCount] = useState(0);
 
+
     let type;
+
+
 
     // Se ejecuta cada vez que se tiene que renderizar.
     useEffect(() => {
         console.log(props.src);
         setLength(audioRef.current.duration);
-
 
         switch (props.type) {
             case 'mp3': type = 'audio/mpeg'; break;
@@ -22,13 +24,18 @@ export default function Audio(props, ...rest) {
         }
 
         console.log("count:", count);
+
     }, [props]);
+    
+    useEffect(()=>{
+        progressInterval = setInterval(()=>{
+            setProgress(Math.round((audioRef.current.currentTime / length) * 100).toString() + "%");
+        },10);
+        return()=>{
+            clearInterval(progressInterval);
+        }
+    },[progress, length])
 
-
-    let timeHandler = () => {
-        setProgress(Math.round((audioRef.current.currentTime / length) * 100).toString() + "%");
-        console.log(progress);
-    }
 
     let changeSound = async () => {
         let response = await fetch("https://api.virtualium.ttde.com.ar/get_sounds_count")
@@ -53,7 +60,7 @@ export default function Audio(props, ...rest) {
             <div className={style.progressBar_container}>
                 <div className={style.progressBar} style={{ width: progress }}></div>
             </div>
-            <audio autoPlay onTimeUpdate={timeHandler} ref={audioRef} onEnded={changeSound}>
+            <audio autoPlay ref={audioRef} onEnded={changeSound}>
                 <source src={props.src +"/"+count} type={type}></source>
             </audio>
         </div>
